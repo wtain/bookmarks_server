cf657b20b36ea0c1c451dad7bcb3ef13d7d75d48
Ramiz <ramiz.zeynalov@gmail.com>
2022-02-13 18:40:07 +0100

    Sync-up

--- src/app/bookmarks/bookmarks.router.ts
+++ src/app/bookmarks/bookmarks.router.ts
@@ -1,12 +1,12 @@
 
 import { Router, Request, Application } from 'express';
 import { MongoClient } from 'mongodb';
-import { BOOKMARKS_ENDPOINT_ADD, BOOKMARKS_ENDPOINT_BASE, BOOKMARKS_ENDPOINT_DELETE, BOOKMARKS_ENDPOINT_UPDATE } from '../../constants/endpoint';
+import { BOOKMARKS_ENDPOINT_ADD, BOOKMARKS_ENDPOINT_BASE, BOOKMARKS_ENDPOINT_DELETE, BOOKMARKS_ENDPOINT_GETBYTAG, BOOKMARKS_ENDPOINT_UPDATE } from '../../constants/endpoint';
 import { COLLECTION_NAME, DB_NAME } from '../../constants/storage';
 
 export const router: Router = Router();
 
-function getBookmarksCollection(req: {app: Application}) {
+export function getBookmarksCollection(req: {app: Application}) {
     const db: MongoClient = req.app.locals.db as MongoClient;
     const bmdb = db.db(DB_NAME);
     const result = bmdb.collection(COLLECTION_NAME);
@@ -35,6 +35,17 @@ router.put(BOOKMARKS_ENDPOINT_UPDATE, async (req, res) => {
 
 router.get(BOOKMARKS_ENDPOINT_BASE + "/", async (req, res) => {
     const bookmarks = await getBookmarksCollection(req).find().toArray();
+    console.info(bookmarks);
+    res.status(200).send(bookmarks);
+  });
+
+
+// db.bookmarks.find({"tags": {$elemMatch: {"name": 'UI'}}})
+
+router.get(BOOKMARKS_ENDPOINT_GETBYTAG + "/", async (req: Request<{tag: string}>, res) => {
+    const bookmarks = await getBookmarksCollection(req)
+                .find({"tags": {"$elemMatch": {"name": req.params.tag}}})
+                .toArray();
     console.info(bookmarks);
     res.status(200).send(bookmarks);
   });
--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -2,3 +2,24 @@
 export const DB_CONN_STRING = "mongodb://localhost:27017"
 export const DB_NAME = "bookmarks_store"
 export const COLLECTION_NAME = "bookmarks"
+
+/*
+
+ db.bookmarks.find({}, {tags: {name: 1}, _id: 0})
+
+  db.bookmarks.aggregate([{ $project: {"tags.name": 1, _id: 0}}, {$unwind: "$tags"}])
+
+    db.bookmarks.aggregate([{ $project: {"tags.name": 1, _id: 0}}, {$unwind: "$tags"}, {$project: {"name": "$tags.name"}}])
+
+db.bookmarks.aggregate([{ $project: {"tags.name": 1, _id: 0}}, {$unwind: "$tags"}, {$project: {"name": "$tags.name"}}, {$group: {_id: null, uniqueValues: {$addToSet: "$name"}}}])
+
+
+db.bookmarks.aggregate([{ $project: {"tags.name": 1, _id: 0}}, {$unwind: "$tags"}, {$project: {"name": "$tags.name"}}, {$group: {_id: null, tags: {$addToSet: "$name"}}}])
+
+
+db.bookmarks.aggregate([{ $project: {"tags.name": 1, _id: 0}}, {$unwind: "$tags"}, {$project: {"name": "$tags.name"}}, {$group: {_id: null, tags: {$addToSet: "$name"}}}, {$project: {tags: 1, _id: 0}}])
+
+
+ db.bookmarks.find({"tags": {$elemMatch: {"name": 'UI'}}})
+
+*/
